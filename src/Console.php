7394a0d2285724511ecb79637918ef0a9d62fb8b
Elan Ruusamäe <glen@pld-linux.org>
2024-12-11 14:52:27 +0200

    Update Console.php: Reduce duplication
    Use fall-through for "false" code branch

--- src/Console.php
+++ src/Console.php
@@ -12,16 +12,12 @@ class Console
     public function execute($args)
     {
         if (count($args) > 1) {
-
             $executable = array_shift($args);
             $commandName = array_shift($args);
 
             if (array_key_exists($commandName, $this->commandsWhitelist)) {
                 return $this->executeCommand($this->commandsWhitelist[$commandName], $args);
             }
-
-            $this->outputHelp();
-            return false;
         }
 
         $this->outputHelp();
